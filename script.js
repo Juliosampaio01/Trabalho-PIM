b6e32e1a2bd50532972b2e3bb2d3fbaf5f375e68
Julio Sampaio <jcesarbisposampaio@gmail.com>
2026-05-22 15:42:53 -0300

    Fix: alterando url do login para o Render

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Espera o HTML carregar
 window.addEventListener("DOMContentLoaded", () => {
 
@@ -14,23 +13,4 @@ window.addEventListener("DOMContentLoaded", () => {
     // Adiciona o script no <head>
     document.head.appendChild(script);
 
-
-=======
-// Espera o HTML carregar
-window.addEventListener("DOMContentLoaded", () => {
-
-    // Cria a tag <script> do VLibras
-    const script = document.createElement("script");
-    script.src = "https://vlibras.gov.br/app/vlibras-plugin.js";
-
-    // Quando terminar de carregar, ativa o widget
-    script.onload = () => {
-        new window.VLibras.Widget('https://vlibras.gov.br/app');
-    };
-
-    // Adiciona o script no <head>
-    document.head.appendChild(script);
-
-
->>>>>>> bf110d567f6f181621370c7983c6161933701340
 });
