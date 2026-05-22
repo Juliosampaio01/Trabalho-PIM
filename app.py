bf110d567f6f181621370c7983c6161933701340
Juliosampaio01 <jcesarbisposampaio@gmail.com>
2026-05-22 01:22:36 -0300

    Pim Criado (site)

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import mysql.connector
+
+app = Flask(__name__)
+CORS(app)
+
+conexao = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Ju20112006",
+    database="saudeplus"
+)
+
+@app.route("/")
+def home():
+    return "Backend conectado ao MySQL!"
+
+@app.route("/cadastro", methods=["POST"])
+def cadastro():
+
+    dados = request.json
+
+    cursor = conexao.cursor()
+
+    sql = """
+    INSERT INTO usuarios (nome, email, telefone, plano, senha)
+    VALUES (%s, %s, %s, %s, %s)
+    """
+
+    valores = (
+        dados["nome"],
+        dados["email"],
+        dados["telefone"],
+        dados["plano"],
+        dados["senha"]
+    )
+
+    cursor.execute(sql, valores)
+    conexao.commit()
+
+    return jsonify({
+        "mensagem": "Usuário cadastrado com sucesso!"
+    })
+@app.route("/login", methods=["POST"])
+def login():
+
+    dados = request.json
+
+    cursor = conexao.cursor(dictionary=True)
+
+    sql = """
+    SELECT * FROM usuarios
+    WHERE email = %s AND senha = %s
+    """
+
+    valores = (
+        dados["email"],
+        dados["senha"]
+    )
+
+    cursor.execute(sql, valores)
+
+    usuario = cursor.fetchone()
+
+    if usuario:
+        return jsonify({
+            "success": True,
+            "usuario": usuario
+        })
+
+    return jsonify({
+        "success": False,
+        "mensagem": "E-mail ou senha incorretos"
+    })
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ script.js
@@ -0,0 +1,17 @@
+// Espera o HTML carregar
+window.addEventListener("DOMContentLoaded", () => {
+
+    // Cria a tag <script> do VLibras
+    const script = document.createElement("script");
+    script.src = "https://vlibras.gov.br/app/vlibras-plugin.js";
+
+    // Quando terminar de carregar, ativa o widget
+    script.onload = () => {
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
+    };
+
+    // Adiciona o script no <head>
+    document.head.appendChild(script);
+
+
+});
